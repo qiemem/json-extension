4b9cc5c9cc8bf292a696aad36b51f17ced4a08a4
Bryan Head <bryan.head@gmail.com>
2013-05-21 17:07:42 -0500

    Switch to make. Add parsing of json lists of strings.

--- src/JSONExtension.scala
+++ src/JSONExtension.scala
@@ -1,9 +1,25 @@
 package org.nlogo.extensions.json
 
-import
-  org.nlogo.api.{ DefaultClassManager, PrimitiveManager }
+import org.nlogo.api._
+import Syntax._
+import ScalaConversions._
+
+import scala.util.parsing.json.JSON
 
 class JSONExtension extends DefaultClassManager {
   def load(primitiveManager: PrimitiveManager) {
+    primitiveManager.addPrimitive("parse", JSONParser)
+  }
+}
+
+object JSONParser extends DefaultReporter {
+  override def getSyntax = reporterSyntax(Array(StringType), ListType)
+  def report(args: Array[Argument], context: Context): AnyRef = {
+    try {
+      JSON.parseFull(args(0).getString) map { case l: List[_] => (l map (_.toString)).toLogoList} getOrElse (throw new ExtensionException("Invalid JSON string"))
+    } catch {
+      case e: LogoException =>
+        throw new ExtensionException(e.getMessage)
+    }
   }
 }
